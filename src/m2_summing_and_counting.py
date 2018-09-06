b8d2bb9b46e8c49be2ee41eaf5a2c891264e4ba4
dotijd <dotijd@rose-hulman.edu>
2018-09-06 16:42:54 -0400

    yuh

--- src/m2_summing_and_counting.py
+++ src/m2_summing_and_counting.py
@@ -7,9 +7,10 @@ A subsequent module lets you practice the ACCUMULATOR pattern in another classic
    IN GRAPHICS:   x = x + pixels
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Jimmy Doti.
+"""  # done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
+import math
 
 # ----------------------------------------------------------------------
 # Students: As you work each of these problems, ask yourself:
@@ -56,6 +57,9 @@ def run_test_sum_more_cosines():
     print('Testing the   sum_more_cosines   function:')
     print('--------------------------------------------------')
 
+
+
+
     # Test 1:
     expected = 0.13416  # This is APPROXIMATELY the correct answer.
     answer = sum_more_cosines(0, 3)
@@ -67,6 +71,17 @@ def run_test_sum_more_cosines():
     # Below this comment, add 2 more test cases of your own choosing.
     # ------------------------------------------------------------------
 
+    #test 2
+    expected = -0.2358 # This is APPROXIMATELY the correct answer.
+    answer = sum_more_cosines(0, 5)
+    print('Test 1 expected:', expected, '(approximately)')
+    print('       actual:  ', answer)
+
+    #test 3
+    expected = .002082  # This is APPROXIMATELY the correct answer.
+    answer = sum_more_cosines( -4, 1)
+    print('Test 1 expected:', expected, '(approximately)')
+    print('       actual:  ', answer)
 
 def sum_more_cosines(m, n):
     """
@@ -83,7 +98,7 @@ def sum_more_cosines(m, n):
          which is approximately 0.02082.
     """
     # ------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # done: 3. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     #   That is called TEST-DRIVEN DEVELOPMENT (TDD).
     #
@@ -95,11 +110,21 @@ def sum_more_cosines(m, n):
     #   Reason: To ensure that you get more practice using variables.
     # ------------------------------------------------------------------
 
+    count = 0
+    for k in range((n-m) + 1):
+       count = math.cos(k + m) + count
+    return count
+
+
+
+
+
+
 
 def run_test_count_sines_from():
     """ Tests the   count_sines_from   function. """
     # ------------------------------------------------------------------
-    # TODO: 4. Implement this TEST function.
+    # done: 4. Implement this TEST function.
     #   It TESTS the  count_sines_from  function defined below.
     #   Include at least **   6   ** tests (we wrote one for you).
     #              ** Yes, 6 (six) tests. **
@@ -117,6 +142,29 @@ def run_test_count_sines_from():
     print('Test 1 expected:', expected)
     print('       actual:  ', answer)
 
+    #test 2
+    expected =
+    answer = count_sines_from(, )
+    print('Test 1 expected:', expected)
+    print('       actual:  ', answer)
+
+    #test 3
+    expected =
+    answer = count_sines_from(, )
+    print('Test 1 expected:', expected)
+    print('       actual:  ', answer)
+
+    #test 4
+    expected =
+    answer = count_sines_from(, )
+    print('Test 1 expected:', expected)
+    print('       actual:  ', answer)
+
+    #test 5
+    expected =
+    answer = count_sines_from(, )
+    print('Test 1 expected:', expected)
+    print('       actual:  ', answer)
     # ------------------------------------------------------------------
     # TO DO: 4 (continued).
     # Below this comment, add 5 more test cases of your own choosing.
@@ -150,6 +198,11 @@ def count_sines_from(m, n):
     #   you must NOT use the 2 or 3-parameter versions
     #   of the RANGE expression, if you happen to know them.
     # ------------------------------------------------------------------
+
+    count = 0
+        for k in range(m, n)
+        count = count
+    return count
 
 
 def run_test_count_sines_vs_cosines():
